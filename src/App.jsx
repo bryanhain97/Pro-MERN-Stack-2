7a673fa16c77cb759d8f2f440a190b31859e3897
Bryan Hain <bryanhain97@gmail.com>
2021-12-05 19:09:29 +0100

    dynamic composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+const issues = [
+    { id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2018-08-15'), due: undefined, title: 'Error in console when clicking Add' },
+    { id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2018-08-16'), due: new Date('2018-08-30'), title: 'Missing bottom border on panel' }
+];
+
 class IssueFilter extends React.Component {
     render() {
         return (
@@ -8,31 +13,40 @@ class IssueFilter extends React.Component {
 
 class IssueRow extends React.Component {
     render() {
-        // couldn't use destructuring assignment. 
-        const style = this.props.style;
-        const issue_id = this.props.issue_id;
+        const issue = this.props.issue;
         return (
             <tr>
-                <td style={style}>{issue_id}</td>
-                <td style={style}>{this.props.children}</td>
+                <td>{issue.id}</td>
+                <td>{issue.status}</td>
+                <td>{issue.owner}</td>
+                <td>{issue.created.toDateString()}</td>
+                <td>{issue.effort}</td>
+                <td>{issue.due ? issue.due.toDateString() : ''}</td>
+                <td>{issue.title}</td>
             </tr>
         )
     }
 }
 class IssueTable extends React.Component {
     render() {
-        const rowStyle = { border: "1px solid silver", padding: 4 };
+        const issueRows = issues.map(issue =>
+            <IssueRow key={issue.id} issue={issue}>{issue.title}</IssueRow>
+        )
         return (
-            <table style={{ borderCollapse: "collapse" }}>
+            <table className="bordered-table">
                 <thead>
                     <tr>
-                        <th style={rowStyle}>ID</th>
-                        <th style={rowStyle}>Title</th>
+                        <th>ID</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Due Date</th>
+                        <th>Title</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <IssueRow style={rowStyle} issue_id={1}>Error in the console when clicking Add</IssueRow>
-                    <IssueRow style={rowStyle} issue_id={2}>Missing bottom border on panel</IssueRow>
+                    {issueRows}
                 </tbody>
             </table>
         )
