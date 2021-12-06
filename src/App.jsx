be472f551d64d751e48b51b032884e7312cecc8c
Bryan Hain <bryanhain97@gmail.com>
2021-12-06 16:41:16 +0100

    stateless functional components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ const initialIssues = [
     { id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2018-08-15'), due: undefined, title: 'Error in console when clicking Add' },
     { id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2018-08-16'), due: new Date('2018-08-30'), title: 'Missing bottom border on panel' }
 ];
-const sampleIssue = {
-    status: 'New', owner: 'Pieta', title: 'Completion date should be optional'
-};
-const sampleIssue2 = {
-    status: 'New', owner: 'Marv', title: 'Completion date should not be optional'
-};
 
 class IssueFilter extends React.Component {
     render() {
@@ -15,57 +9,63 @@ class IssueFilter extends React.Component {
             <div>This is a placeholder for the issue filter.</div>
         )
     }
+};
+const IssueRow = (props) => {
+    const issue = props.issue;
+    return (
+        <tr>
+            <td>{issue.id}</td>
+            <td>{issue.status}</td>
+            <td>{issue.owner}</td>
+            <td>{issue.created.toDateString()}</td>
+            <td>{issue.effort}</td>
+            <td>{issue.due}</td>
+            <td>{issue.title}</td>
+        </tr>
+    )
 }
-class IssueRow extends React.Component {
-    render() {
-        const issue = this.props.issue;
-        return (
-            <tr>
-                <td>{issue.id}</td>
-                <td>{issue.status}</td>
-                <td>{issue.owner}</td>
-                <td>{issue.created.toDateString()}</td>
-                <td>{issue.effort}</td>
-                <td>{issue.due ? issue.due.toDateString() : ''}</td>
-                <td>{issue.title}</td>
-            </tr>
-        )
-    }
-}
-class IssueTable extends React.Component {
-    render() {
-        let issueRows = this.props.issues.map(issue => (
-            <IssueRow issue={issue} key={issue.id} />
-        ))
-        return (
-            <table className="bordered-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Status</th>
-                        <th>Owner</th>
-                        <th>Created</th>
-                        <th>Effort</th>
-                        <th>Due Date</th>
-                        <th>Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {issueRows}
-                </tbody>
-            </table>
-        )
-    }
-}
+const IssueTable = (props) => {
+    const issueRows = props.issues.map(issue => (
+        <IssueRow issue={issue} key={issue.id} />
+    ));
+    return (
+        <table className="bordered-table">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Status</td>
+                    <td>Owner</td>
+                    <td>Created</td>
+                    <td>Effort</td>
+                    <td>Due Date</td>
+                    <td>Title</td>
+                </tr>
+            </thead>
+            <tbody>
+                {issueRows}
+            </tbody>
+        </table>
+    )
+};
 class IssueAdd extends React.Component {
     constructor() {
         super();
-        setTimeout(() => this.props.createIssue(sampleIssue), 1500);
-        setTimeout(() => this.props.createIssue(sampleIssue2), 3000);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const form = document.forms.issueAdd;
+        const issue = { owner: form.owner.value, title: form.title.value, status: 'New' };
+        this.props.createIssue(issue);
+        form.owner.value = ""; form.title.value = "";
     }
     render() {
         return (
-            <div>This is a placeholder for a form to add an issue.</div>
+            <form name="issueAdd" onSubmit={this.handleSubmit}>
+                <input type="text" name="owner" placeholder="Owner" />
+                <input type="text" name="title" placeholder="Title" />
+                <button>Add</button>
+            </form>
         )
     }
 }
