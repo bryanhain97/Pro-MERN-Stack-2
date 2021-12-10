6707fef746a0d69d4f0edc8c735367472b10ee46
Bryan Hain <bryanhain97@gmail.com>
2021-12-10 22:19:40 +0100

    mod nodemon -e graphql

--- server/server.js
+++ server/server.js
@@ -3,7 +3,19 @@ const fs = require('fs');
 const { ApolloServer } = require('apollo-server-express');
 const app = express();
 
+let aboutMessage = "Issue Tracker API v1.0";
 
+const resolvers = {
+    Query: {
+        about: () => aboutMessage
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+};
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+};
 
 const GraphQLServer = new ApolloServer({
     typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
