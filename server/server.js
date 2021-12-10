20fb25496651d39a598f041ac6c371f49f4e9d5a
Bryan Hain <bryanhain97@gmail.com>
2021-12-10 13:53:20 +0100

    initialize GraphQL server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+const typeDefs = `
+type Query {
+    about: String!
+}
+type Mutation {
+    setAboutMessage(message: String!): String
+}
+`;
+const resolvers = {
+    Query: {
+        about = () => aboutMessage
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+};
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+const GraphQLServer = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 
-const fileServerMiddleware = express.static('./public');
-app.use(fileServerMiddleware);
-app.listen(3000, () => console.log(`App is running on PORT 3000`))
+app.use(express.static('./public'));
+
+GraphQLServer.applyMiddleware({ app, path: '/graphql' });
+app.listen(3000, () => console.log(`App started on PORT 3000`));
