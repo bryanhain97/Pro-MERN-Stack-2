2ea2802c608024ed5a794fb8c34f20564cf3717e
Bryan Hain <bryanhain97@gmail.com>
2021-12-13 18:53:03 +0100

    stringify Date objects

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const app = express();
 
 let aboutMessage = "Issue Tracker API v1.0";
 const issueDB = [
-    { id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2019-01-15'), due: undefined, title: 'Error in console when clicking Add' },
-    { id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2019-01-16'), due: new Date('2019-02-01'), title: 'Missing bottom border on panel' }
+    { id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2019-01-15').toString(), due: undefined, title: 'Error in console when clicking Add' },
+    { id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2019-01-16').toString(), due: new Date('2019-02-01').toString(), title: 'Missing bottom border on panel' }
 ]
 const resolvers = {
     Query: {
