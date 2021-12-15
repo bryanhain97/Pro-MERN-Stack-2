6d54a66fcd9132e410e140eadb89f42adc91b590
Bryan Hain <bryanhain97@gmail.com>
2021-12-15 23:53:20 +0100

    graphql scalar

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const fs = require('fs');
 const { ApolloServer } = require('apollo-server-express');
+const { GraphQLScalarType } = require('graphql');
 const app = express();
+
+const GraphQLDate = new GraphQLScalarType({
+    name: 'GraphQLDate',
+    description: 'A Date() type in GraphQL as a scalar',
+    serialize(value) {
+        return value.toISOString();
+    },
+
+})
 
 let aboutMessage = "Issue Tracker API v1.0";
 const issueDB = [
-    { id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2019-01-15').toString(), due: undefined, title: 'Error in console when clicking Add' },
-    { id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2019-01-16').toString(), due: new Date('2019-02-01').toDateString(), title: 'Missing bottom border on panel' }
+    { id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2019-01-15'), due: undefined, title: 'Error in console when clicking Add' },
+    { id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2019-01-16'), due: new Date('2019-02-01'), title: 'Missing bottom border on panel' }
 ]
 const resolvers = {
     Query: {
@@ -16,6 +26,7 @@ const resolvers = {
     Mutation: {
         setAboutMessage,
     },
+    GraphQLDate
 };
 function issueList() {
     return issueDB;
