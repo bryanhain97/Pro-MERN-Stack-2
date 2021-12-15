56ee7ca1c9b3bb9ae5ae4f2d02130cad2fb8fd57
Bryan Hain <bryanhain97@gmail.com>
2021-12-15 10:25:51 +0100

    due to date string

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 let aboutMessage = "Issue Tracker API v1.0";
 const issueDB = [
     { id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2019-01-15').toString(), due: undefined, title: 'Error in console when clicking Add' },
-    { id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2019-01-16').toString(), due: new Date('2019-02-01').toString(), title: 'Missing bottom border on panel' }
+    { id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2019-01-16').toString(), due: new Date('2019-02-01').toDateString(), title: 'Missing bottom border on panel' }
 ]
 const resolvers = {
     Query: {
